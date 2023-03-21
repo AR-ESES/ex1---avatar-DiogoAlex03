ada86fee60bdafbbf99d97bf829b6ed0cade7a04
DiogoAlex03 <128501698+DiogoAlex03@users.noreply.github.com>
2023-03-21 17:27:51 +0000

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -13,4 +13,4 @@ function draw() {
 	circle(width / 2 + height /3/4, height /2 - height/18, height/12 );
 	line(450,600,400,500);
 	}
-no editor a boca está dentro da cara, mas no meu computador está fora
+<!-- no editor a boca está dentro da cara, mas no meu computador está fora -->
