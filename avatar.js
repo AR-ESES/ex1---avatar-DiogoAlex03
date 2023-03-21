642507b1dbde70c1b6d6636af04d9d1a631fc1e4
DiogoAlex03 <128501698+DiogoAlex03@users.noreply.github.com>
2023-03-21 17:26:45 +0000

    Update and rename sketch.js to avatar.js

--- avatar.js
+++ avatar.js
@@ -13,3 +13,4 @@ function draw() {
 	circle(width / 2 + height /3/4, height /2 - height/18, height/12 );
 	line(450,600,400,500);
 	}
+no editor a boca está dentro da cara, mas no meu computador está fora
